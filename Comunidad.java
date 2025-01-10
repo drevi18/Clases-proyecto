210b0eb1120173749b9f6fc22d4b4dc63dc3302e
Sara <158341971+drevi18@users.noreply.github.com>
2025-01-10 18:44:52 +0100

    Create Comunidad.java

---
+++ Comunidad.java
@@ -0,0 +1,19 @@
+public class Comunidad {
+    private int codigo;
+
+    public Comunidad(){
+        this.codigo=0;
+    }
+    public Comunidad(int codigo){
+        this.codigo=codigo;
+
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}

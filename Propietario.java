ca8a691457beaa919aec024ce628c8dfe24ee653
Sara <158341971+drevi18@users.noreply.github.com>
2025-01-10 18:46:47 +0100

    Create Propietario.java

---
+++ Propietario.java
@@ -0,0 +1,20 @@
+public class Propietario {
+    private int DNI;
+
+    public Propietario(){
+        this.DNI=0;
+    }
+    public Propietario(int DNI){
+        this.DNI=DNI;
+
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+    
+}

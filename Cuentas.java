e74e8304cff42183cfbf9194faf66743161e0a5c
Sara <158341971+drevi18@users.noreply.github.com>
2025-01-10 18:45:13 +0100

    Create Cuentas.java

---
+++ Cuentas.java
@@ -0,0 +1,20 @@
+public class Cuentas {
+    private String bancos;
+
+    public Cuentas(){
+        this.bancos=" ";
+    }
+    public Cuentas(String bancos){
+        this.bancos=bancos;
+    }
+
+    public String getBancos() {
+        return bancos;
+    }
+
+    public void setBancos(String bancos) {
+        this.bancos = bancos;
+    }
+
+    
+}
